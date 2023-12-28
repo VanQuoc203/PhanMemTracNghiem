4aaa8141de91f5d82e5dbac3828360c41c5d37cf
VanQuoc203 <vanquoc203nt@gmail.com>
2023-12-28 10:55:38 +0700

    first commit

---
+++ app/src/main/java/com/example/quanlytracnghiem/activitys/ThemVPKhoa_activitys.java
@@ -0,0 +1,16 @@
+package com.example.quanlytracnghiem.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quanlytracnghiem.R;
+
+public class ThemVPKhoa_activitys extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.them_vpkhoa_layout);
+    }
+}
